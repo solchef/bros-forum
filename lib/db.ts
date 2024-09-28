fce8a268c9352c7be6b474cdb1661ab3356b1666
jchege <jawiwy@gmail.com>
2024-09-28 19:04:40 +0400

    sort out prisma error

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
+
+export const db = () => {}
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
